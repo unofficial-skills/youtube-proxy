15df067c9057bc965a446f1b50c0692f1ead1177
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2019-11-20 21:16:35 +0100

    Move index.js into src folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-'use strict';
-
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/ping', (req, res) => {
-  return res.json({
+app.get('/ping', (req, res) =>
+  res.json({
     message: 'pong'
-  });
-});
+  })
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port} in ${app.get('env')} mode over HTTP`);
