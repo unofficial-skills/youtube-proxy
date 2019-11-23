14b93ed4286947adc7f15d89db6c4ddce3ef0ad8
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2019-11-23 11:35:29 +0100

    Add play API module and endpoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+// API modules
+const playApi = require('./api/play');
 const searchApi = require('./api/search');
 
 const app = express();
@@ -10,7 +13,14 @@ app.get('/', (req, res) =>
   })
 );
 
+// /api/search?query=ABC (default limit 1)
+// /api/search?query=ABC&limit=5
 app.get('/api/search', searchApi);
+
+// /api/play/:id (default type video)
+// /api/play/:id?type=video
+// /api/play/:id?type=audio
+app.get('/api/play/:id', playApi);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port} in ${app.get('env')} mode over HTTP`);
